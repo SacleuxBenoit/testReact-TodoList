ead5ca537aab860d7edaef49dab7e7d44743d116
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-10 14:22:47 +0100

    add content to the h1 in Todo/Todolist.js

--- todolist/src/Todo/TodoList.js
+++ todolist/src/Todo/TodoList.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class TodoList extends Component {
     render(){
         return(
-            <div><h1></h1></div>
+            <div><h1>Ma TodoList</h1></div>
         )
     }
 }
