f7bf860c19d8a9bf6a1c94630d6f638970c31699
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-10 14:27:00 +0100

    add an input in Todo/TodoList.js

--- todolist/src/Todo/TodoList.js
+++ todolist/src/Todo/TodoList.js
@@ -3,8 +3,12 @@ import React, {Component} from 'react';
 class TodoList extends Component {
     render(){
         return(
-            <div><h1>Ma TodoList</h1></div>
-        )
+            <div><h1>Ma TodoList</h1>
+            <form>
+                <input type="text" placeholder="Renseigner un item"></input>
+            </form>
+            </div>
+        );
     }
 }
 
