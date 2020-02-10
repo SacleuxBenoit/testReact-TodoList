d0cac2e5530d387dcf2da296f1e81f851ca0dab6
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-10 14:34:45 +0100

    add onChange in the input of Todo.TodoList.js

--- todolist/src/Todo/TodoList.js
+++ todolist/src/Todo/TodoList.js
@@ -12,7 +12,13 @@ class TodoList extends Component {
         return(
             <div><h1>Ma TodoList</h1>
             <form>
-                <input value={this.state.userInput}type="text" placeholder="Renseigner un item"></input>
+                <input 
+                    value={this.state.userInput}
+                    type="text" 
+                    placeholder="Renseigner un item"
+                    onChange={this.onChange.bind(this)} 
+                >
+                </input>
                 <button>Ajouter</button>
             </form>
             </div>
